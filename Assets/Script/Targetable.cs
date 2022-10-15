15fe0ebc03e0a597e9750b7454a8101607d6aae7
Pitty231 <106615050+Pitty231@users.noreply.github.com>
2022-10-15 13:40:50 -0300

    kijh
    ijkh

---
+++ Assets/Script/Targetable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targetable : MonoBehaviour
+{
+    public enum EnemyType { Minion, Torre1 }
+    public EnemyType enemyType;
+}
