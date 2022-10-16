ccb5c5cefea31cfb82b3196fefe709d23bef62cb
Pitty231 <106615050+Pitty231@users.noreply.github.com>
2022-10-16 13:19:21 -0300

    .
    .

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     public NavMeshAgent agente;
-
+    public Transform posicaoDest;
     public float rotateSpeedMovement = 0.075f;
     float rotateVelocity;
 
@@ -19,20 +19,34 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        raycastMover();
+    }
+    void raycastMover()
+    {
+        if (Input.GetMouseButton(1))
         {
             RaycastHit hit;
 
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
                 // Movimento
                 agente.SetDestination(hit.point);
+                posicaoDest.position = hit.point;
 
                 // Rotacao
                 Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
                 float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y, ref rotateVelocity, rotateSpeedMovement * (Time.deltaTime * 5));
 
-                transform.eulerAngles = new Vector3(0,rotationY,0);
+                transform.eulerAngles = new Vector3(0, rotationY, 0);
+                {
+                }
+            }
+        }
+        void controlAnim()
+        {
+            if (Vector3.Distance(this.gameObject.transform.position, posicaoDest.position) <= 2)
+            {
+
             }
         }
     }
